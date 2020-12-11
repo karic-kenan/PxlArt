dee2fbae9285d066c12c6f42e7ea7a551ad859e9
Kenan <kenan.karic@outlook.com>
2020-12-11 09:37:11 +0100

    Project setup

---
+++ app/src/main/java/io/aethibo/pxlart/MainActivity.kt
@@ -0,0 +1,11 @@
+package io.aethibo.pxlart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
