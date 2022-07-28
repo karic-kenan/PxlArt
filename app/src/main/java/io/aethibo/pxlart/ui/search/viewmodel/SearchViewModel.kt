85efc76a270373378dc0b9777625487560b85103
Kenan <kenan.karic@outlook.com>
2022-07-28 09:09:16 +0200

    Updated dependencies and gradle version

--- app/src/main/java/io/aethibo/pxlart/ui/search/viewmodel/SearchViewModel.kt
+++ app/src/main/java/io/aethibo/pxlart/ui/search/viewmodel/SearchViewModel.kt
@@ -2,12 +2,15 @@ package io.aethibo.pxlart.ui.search.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import io.aethibo.pxlart.domain.Photo
 import io.aethibo.pxlart.usecases.search.ISearchUseCase
+import kotlinx.coroutines.flow.Flow
 
 class SearchViewModel(private val useCase: ISearchUseCase) : ViewModel() {
 
     /**
      * Search for photos
      */
-    fun getSearchResult(query: String) = useCase.getSearchResultStream(query, viewModelScope)
+    fun getSearchResult(query: String): Flow<PagingData<Photo>> = useCase.getSearchResultStream(query, viewModelScope)
 }
